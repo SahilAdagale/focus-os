88eebe47e8ecb8600a209d62d432fada0e08d395
Sahil Adagale <sahiladagale16@gmail.com>
2026-05-27 10:35:08 +0530

    feat: implement server-side user authentication with MongoDB, JWT, and Postman workspace configuration

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,72 @@
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+
 const registerUser = async (req, res) => {
-    console.log(req.body);
+    try {
+        const { name, email, password } = req.body;
+
+        const existUser = await User.findOne({ email });
+
+        if (existUser) {
+            return res.status(400).json({ message: "User already exists" })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = new User({
+            name,
+            email,
+            password: hashedPassword
+        })
+
+        await user.save();
+        res.status(201).json({ message: "User created successfully", user: { name: user.name, email: user.email, password: user.password } })
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: "Internal server error" })
+    }
 }
 
-module.exports = { registerUser };
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(401).json({ message: "User not found" })
+        }
+
+        const isPassValid = await bcrypt.compare(password, user.password);
+
+        if (!isPassValid) {
+            return res.status(401).json({ message: "Invalid Credentials" })
+        }
+
+        const token = jwt.sign(
+            { id: user._id, role: user.role },
+            process.env.JWT_SECRET,
+            { expiresIn: "1h" }
+        );
+
+        res.status(200).json({
+            message: "Login success",
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            },
+            token
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Internal server error" })
+    }
+}
+
+module.exports = { registerUser, loginUser };
